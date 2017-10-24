e31bb43418ae2ebf4d6dd822e240b11da014d9c7
Ashlee Krammer <ashlee.alyse@me.com>
2017-10-24 17:55:26 -0400

    First Commit

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -9,4 +9,50 @@
 import Foundation
 
 class TextTwistModel {
+    
+    enum trueOrFalse {
+        case correct, wrong, alreadyGuessed
+    }
+    
+    //something that randomizes the word being chosen and adds it to word label
+    var currentInfo = WordData.allInfo[Int(arc4random_uniform(3))]
+
+    //something that checks if user try is valid and then puts it in a box if true, says no if false
+    func wordIsCorrect(usersWord: String) -> trueOrFalse {
+        if currentInfo.words.contains(usersWord) {
+            return .correct
+        } else {
+            return .wrong
+        }
+    }
+    
+    //something that makes keyboard disapear on return
+    //something that takes letter used away from word label and adds back when delted from input
+    //if all options of word are selected then push a new word and restart
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -8,7 +8,105 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate  {
+    
+    
+    
+    //UIView Stuff
+    // Word Bank Label
+    @IBOutlet weak var wordBankLabel: UILabel!
+    
+    // Guessing Box
+    @IBOutlet weak var guessingBox: UITextField!
+    
+    // Message Label
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    // 3,4,5,6 Word Banks
+    @IBOutlet weak var threeWordBank: UITextView!
+    @IBOutlet weak var fourWordBank: UITextView!
+    @IBOutlet weak var fiveWordBank: UITextView!
+    @IBOutlet weak var sixWordBank: UITextView!
+    
+    var model = TextTwistModel() //lets you access the model
+    
+    
+   override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //call funcs to do all that stuff with the outlets above
+    
+    //calls func to see if users input is correct
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //wordBankLabel.isHidden = false
+        wordBankLabel.text = model.currentInfo.letters
+        
+        print(model.currentInfo.letters)
+        
+        guard let guessedWord = guessingBox.text else {
+            return false
+        }
+        
+        messageLabel.text = "Hey"
+        
+        //Changes messages based on whether word is good
+        switch model.wordIsCorrect(usersWord: guessedWord) {
+        case .correct:
+         messageLabel.text = "Yay you did some shit"
+        case .wrong:
+            messageLabel.text = "You did shit but this word is wrong"
+        case .alreadyGuessed:
+            messageLabel.text = "You already wrote this"
+        }
+        return true
+        
+//        if model.wordIsCorrect(usersWord: guessedWord) == .correct {
+//            //messageLabel.isHidden = false
+//            messageLabel.text = "Yay you did some shit"
+//            return true
+//
+//        } else if model.wordIsCorrect(usersWord: guessedWord) == .wrong {
+//            //messageLabel.isHidden = false
+//            messageLabel.text = "You did shit but this word is wrong"
+//            return false
+//
+//        } else {
+//            return false
+//        }
+//
+//        return true
+    }
+    
+    
 }
+
+
+
+
+
+
+//        //Changes messages based on whether word is good
+//        if model.wordIsCorrect(usersWord: guessingBox.text!) == .correct {
+//            print(messageLabel.text = "Yay you did some shit")
+//            return true
+//        } else if model.wordIsCorrect(usersWord: guessingBox.text!) == .wrong {
+//            print(messageLabel.text = "You did shit but this word is wrong")
+//            return false
+//        } else {
+//            return false
+//        }
+
 
